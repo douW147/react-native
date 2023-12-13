71ddcb6d50b368f004aa3996f727cd99b78b49fc
Alex the wizard <sashok1111122222@gmail.com>
2023-12-13 12:05:57 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function App(): React.ReactElement {
+  return (
+    <>
+      <Text>
+        Test screen Test screen Test screen Test screen Test 
+        screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test 
+        screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test 
+        screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test 
+        screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screenTest screen Test screen Test screen Test screen Test 
+        screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+        Test screen Test screen Test screen Test screen Test screen
+      </Text>
+    </>
+  );
+}
+
+export default App;
